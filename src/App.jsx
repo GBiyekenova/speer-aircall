6b1217450177c871db0b8302137ea3ee8bec711f
gbiyekenova <gbknva@gmail.com>
2023-04-30 11:35:06 -0400

    added activity feed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
 
-import Header from './Header.jsx';
+import Header from "./Header.jsx";
+import ActivityFeed from "./ActivityFeed.jsx";
+import Footer from "./Footer.jsx";
+
+const baseURL = "https://cerulean-marlin-wig.cyclic.app/";
 
 const App = () => {
   return (
-    <div className='container'>
-      <Header/>
-      <div className="container-view">Some activities should be here</div>
+    <div className="container">
+      <Header />
+      {/* <ActivityFeed /> */}
+      <div className="container-view">
+        <ActivityFeed />
+        <Footer />
+      </div>
     </div>
   );
 };
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
 
 export default App;
